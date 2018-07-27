a448f7f3ae9021334ae6b2d7509e4da092fe2991
Deelawn Peck <dylanmpeck@gmail.com>
2018-07-27 01:21:12 -0700

    Reorganized C++ classes: Deleted empty Projectile Movement class and Moved Player Controller into appropriate folder without breaking.

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Projectile.h"
-#include "ProjectileMovement.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 
 
@@ -11,7 +10,7 @@ AProjectile::AProjectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
     
-    ProjectileMovement = CreateDefaultSubobject<UProjectileMovement>(FName("Projectile Movement Component"));
+    ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement Component"));
     ProjectileMovement->bAutoActivate = false;
 }
 
